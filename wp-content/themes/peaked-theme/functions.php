26f4a088f6e0b333b38e272b3618654b0d06d0ba
devcarlan <devcarlan@gmail.com>
2019-07-14 12:44:02 -0400

    adding navigation

---
+++ wp-content/themes/peaked-theme/functions.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Enqueue scripts and styles.
+ */
+
+ /* Add Custom CSS */
+ function peaked_theme_scripts() {
+ 	wp_enqueue_style( 'peaked-theme-style', get_stylesheet_uri() );
+
+ 	/* Add Custom CSS */
+ 	wp_enqueue_style( 'peaked-theme-custom-style', get_stylesheet_directory_uri() . '/css/style.css', array(), '1' );
+
+ 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+ 		wp_enqueue_script( 'comment-reply' );
+ 	}
+ }
+ add_action( 'wp_enqueue_scripts', 'peaked_theme_scripts');
+
+ ?>
--- wp-content/themes/peaked-theme/index.php
+++ wp-content/themes/peaked-theme/index.php
@@ -1,1 +1,30 @@
-this be working
+  <?php
+  /**
+   * Peaked home page template.
+   *
+   */
+  get_header(); ?>
+
+  <body>
+    <!-- Navigation -->
+    <div class="container">
+      <nav class="navbar navbar-expand-lg">
+        <a class="navbar-brand" href="/"><span id="peak">Peaked</span> Gaming</a>
+        <div class="" id="navbarNav">
+          <ul class="navbar-nav">
+           <li class="nav-item active"><a class="nav-link" href="/">Home</a></li>
+           <li class="nav-item"><a class="nav-link" href="/about">About</a></li>
+      	   <li class="nav-item"><a class="nav-link" href="/partner">Partners</a></li>
+           <li class="nav-item"><a class="nav-link" href="/shop">Shop</a></li>
+      	   <li class="nav-item"><a class="nav-link" href="/contact">Contact</a></li>
+          </ul>
+        </div>
+
+        <ul class="navbar-nav ml-auto">
+          <li><a class="nav-link" href="#"><i class="fas fa-user"></i> Register </a></li>
+          <li><a class="nav-link" href="#"><i class="fas fa-sign-in-alt"></i> Login </a></li>
+        </ul>
+      </nav>
+    </div>
+    <?php get_footer(); ?>
+  </body>
