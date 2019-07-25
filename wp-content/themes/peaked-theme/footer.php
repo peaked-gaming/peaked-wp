ae0bfee486fe97b4eafdfb96dd32cc12907ca8e9
devcarlan <devcarlan@gmail.com>
2019-07-24 22:03:58 -0400

    adding profile pcitures as well as socials and sponser area

---
+++ wp-content/themes/peaked-theme/footer.php
@@ -0,0 +1,29 @@
+    <div id="footer" class="container">
+      <div class="row">
+          <div class="col-lg-6 h-100 text-left mr-auto">
+            <ul class="list-inline mb-2">
+              <li class="list-inline-item"><a href="/about">About</a></li>
+              <li class="list-inline-item">&sdot;</li>
+              <li class="list-inline-item"><a href="/contact">Contact</a></li>
+              <li class="list-inline-item">&sdot;</li>
+              <li class="list-inline-item"><a href="/terms">Terms of Use</a></li>
+              <li class="list-inline-item">&sdot;</li>
+              <li class="list-inline-item"><a href="/privacy">Privacy Policy</a></li>
+            </ul>
+            <p class="text-muted small mb-4">&copy; Peaked Gaming 2019. All Rights Reserved.</p>
+          </div>
+          <div class="col-lg-6 h-100 text-right ml-auto">
+            <ul class="list-inline mb-0">
+              <li class="list-inline-item mr-3"><a href="https://www.patreon.com/peakedgg"><i class="fab fa-paypal fa-2x fa-fw"></i></a></li>
+              <li class="list-inline-item mr-3"><a href="https://twitter.com/peakedgg"><i class="fab fa-twitter-square fa-2x fa-fw"></i></a></li>
+              <li class="list-inline-item"><a href="https://www.youtube.com/channel/UCkeMouYnTvPKaPvSShnSDmg"><i class="fab fa-youtube fa-2x fa-fw"></i></a></li>
+              <li class="list-inline-item"><a href="https://www.instagram.com/peakedgaming/"><i class="fab fa-instagram fa-2x fa-fw"></i></a></li>
+              <li class="list-inline-item"><a href="https://www.facebook.com/PeakedGG"><i class="fab fa-facebook fa-2x fa-fw"></i></a></li>
+              <li class="list-inline-item"><a href="https://steamcommunity.com/groups/PeakedGG"><i class="fab fa-steam fa-2x fa-fw"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
---
+++ wp-content/themes/peaked-theme/page-profile.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Template Name: Profile
+ * Streamer or Patreon Supporter Profile.
+ *
+ */
+
+get_header(); ?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-12 text-center">
+      <img id="profilepic" class="img-fluid" src="https://via.placeholder.com/200x200" />
+      <h1 class="text-center bodyh1">Name</h1>
+    </div>
+  </div>
+</div>
+
+<div class="container">
+  <div id="profilesocials" class="row text-center">
+    <div class="col-md-3">
+      <img class="img-fluid" src="https://via.placeholder.com/200x50" />
+    </div>
+    <div class="col-md-3">
+      <img class="img-fluid" src="https://via.placeholder.com/200x50" />
+    </div>
+    <div class="col-md-3">
+      <img class="img-fluid" src="https://via.placeholder.com/200x50" />
+    </div>
+    <div class="col-md-3">
+      <img class="img-fluid" src="https://via.placeholder.com/200x50" />
+    </div>
+  </div>
+</div>
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-12 text-center">
+      <p></p>
+    </div>
+  </div>
+</div>
+
+
+<?php get_footer(); ?>
